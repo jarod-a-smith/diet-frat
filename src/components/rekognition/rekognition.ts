dd5778cd4a130fa169600829a449305307dc753c
ferrantejake <jake.ferrante@hotmail.com>
2018-04-07 14:35:54 -0400

    Add Amazon components wireframe

---
+++ src/components/rekognition/rekognition.ts
@@ -0,0 +1,17 @@
+/**
+ *
+ * @param ids S3 Bucket Ids
+ */
+export function classifyImages(bucketId: string) {
+    /**
+     * Go out to S3
+     * Get images
+     * Send to Rekognition
+     * Get back classified images (and their bounding boxes)
+     */
+}
+export function getOriginalImages(ids: string[]) {
+    /**
+     * Get original image for id in `ids` from S3
+     */
+}
